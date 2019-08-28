df9ab972816d0af2b96c8371cac5efffcab42f0d
韩瑞东 <hanruidong@meituan.com>
2019-08-28 20:37:23 +0800

    支持没有归一化形式的输入

--- orthogonal.py
+++ orthogonal.py
@@ -17,6 +17,8 @@ def orthogonalRotate(Y, L1, L2, alpha, normalization = False):
     :param alpha: float, weight of L2 vector
     :param normalization: bool, normalize input data
     :return: array [n,n], best orthogonal matrix T
+
+    Tips : suggest normalize the input vector
     '''
     L1 = np.array(L1)
     L2 = np.array(L2)
@@ -26,7 +28,12 @@ def orthogonalRotate(Y, L1, L2, alpha, normalization = False):
         L1 = normalize(L1)
         L2 = normalize(L2)
 
-    Y_target = Y*(1+alpha) - L1
+    m = Y.shape[0]
+    for i in range(m):
+        target = np.sqrt(np.sum(L1[i,:]**2)) + alpha*np.sqrt(np.sum(L2[i,:]**2))
+        Y[i,:] = Y[i,:]*target
+
+    Y_target = Y - L1
     X_target = L2*alpha
     A = X_target.T.dot(Y_target)
     U,sigma,VT=la.svd(A)
@@ -40,12 +47,14 @@ if __name__ == "__main__":
     L1 = np.array([[0.1,0.6,0.3],[0.2,0.4,0.2],[0.2,0.1,0.8]],dtype='float32')
     L2 = np.array([[0.4,0.5,0.2],[0.7,0.1,0.2],[0.4,0.2,0.3]],dtype='float32')
     alpha = 0.5
-    L1 = normalize(L1)
-    L2 = normalize(L2)
     rotate_matrix = orthogonalRotate(Y, L1, L2, alpha, True)
-    loss_1 = np.sum(((L1 + alpha*L2) - Y*(1+alpha))**2)
-    print('origin predict\n',L1 + alpha*L2)
+    m = Y.shape[0]
+    for i in range(m):
+        target = np.sqrt(np.sum(L1[i, :] ** 2)) + alpha * np.sqrt(np.sum(L2[i, :] ** 2))
+        Y[i, :] = Y[i, :] * target
+    loss_1 = np.sum(((L1 + alpha*L2) - Y)**2)
+    # print('origin predict:\n',L1 + alpha*L2)
     print('origin loss',loss_1)
-    loss_2 = np.sum(((L1 + alpha*L2.dot(rotate_matrix)) - Y*(1+alpha))**2)
-    print('rotate predict\n',L1 + alpha*L2.dot(rotate_matrix))
+    loss_2 = np.sum(((L1 + alpha*L2.dot(rotate_matrix)) - Y)**2)
+    # print('rotate predict:\n',L1 + alpha*L2.dot(rotate_matrix))
     print('rotate loss',loss_2)
